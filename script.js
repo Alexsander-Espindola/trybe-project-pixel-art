b46692b6208f4c5eb6bf54c1521bf1d85feef2aa
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-06-02 16:42:51 -0300

    Primeiro e segundo desafio completo

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-let docType = document.getElementById('table');
+const docType = document.getElementById('color-palette');
 
-function createBox (className){
-  let box = document.createElement('div');
+function createBox(className) {
+  const box = document.createElement('div');
   box.className = className;
   return box;
 }
 
-let const = document.getElementById('table');
-console.log(const);
-
-function createMatriz (divLine, divColumn){
-  for(let i = 0; i < divLine; i += 1){
-    let line = createBox('tr');
+function createMatriz(divLine, divColumn, trClassName, tdClassName) {
+  for (let i = 0; i < divLine; i += 1) {
+    const line = createBox(`tr ${trClassName}`);
     docType.appendChild(line);
-    for(let j = 0; j < divColumn; j += 1 ){
-      let column = createBox('td box');
+    for (let j = 0; j < divColumn; j += 1) {
+      const column = createBox(`td ${tdClassName}`);
       line.appendChild(column);
     }
   }
 }
-createMatriz(1, 4);
-let collorPalette = document.getElementsByClassName('tr');
-collorPalette[0].className = 'tr color';
+createMatriz(1, 4, '', ' color box');
+const boxColor = document.getElementsByClassName('color');
 
-createMatriz(5, 5);
+for (let index = 0; index < boxColor.length; index += 1) {
+  const colorRed = Math.round(Math.random() * 255);
+  const colorBlue = Math.round(Math.random() * 255);
+  const colorGreen = Math.round(Math.random() * 255);
+  boxColor[index].style.backgroundColor = `rgb( ${colorRed}, ${colorGreen}, ${colorBlue})`;
+
+}
+createMatriz(5, 5, ' box', ' box');
