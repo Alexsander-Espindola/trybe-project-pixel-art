072e8987741b9316b0e520c41a20b018fec57c17
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-06-02 14:33:53 -0300

    teste

--- script.js
+++ script.js
@@ -1,44 +1,26 @@
-window.onload = function (){
-  let docType = document.getElementById("table");
+let docType = document.getElementById('table');
 
-  function createBox (className){
-    let box = document.createElement("div");
-    box.className = className;
-    return box;
-  }
+function createBox (className){
+  let box = document.createElement('div');
+  box.className = className;
+  return box;
+}
 
-  let table = document.getElementById('table');
-  console.log(table);
+let const = document.getElementById('table');
+console.log(const);
 
-  function createMatriz (divLine, divColumn) {
-    for(let i = 0; i < divLine; i += 1){
-      let line = createBox("tr");
-      docType.appendChild(line);
-      for(let j = 0; j < divColumn; j += 1 ){
-            let column = createBox("td box");
-            line.appendChild(column);
-      }
+function createMatriz (divLine, divColumn){
+  for(let i = 0; i < divLine; i += 1){
+    let line = createBox('tr');
+    docType.appendChild(line);
+    for(let j = 0; j < divColumn; j += 1 ){
+      let column = createBox('td box');
+      line.appendChild(column);
     }
   }
-
-  createMatriz(1, 4);
-  let collorPalette = document.getElementsByClassName("tr");
-  collorPalette[0].className = "tr color"
-
-  createMatriz(5, 5);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+createMatriz(1, 4);
+let collorPalette = document.getElementsByClassName('tr');
+collorPalette[0].className = 'tr color';
+
+createMatriz(5, 5);
