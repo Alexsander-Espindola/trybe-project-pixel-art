98980b8cf2c9db68ce9fa772a13e33898f4e2153
Alexsander Espindola <alexsander81002745@gmail.com>
2022-06-22 14:30:04 -0300

    Organizando o código em duas pastas, a utilities para as funções de utilidades e o script para o uso dessas funções

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-import CreateMatriz from "./utilities.js";
-// function createBox(className) {
-//   const box = document.createElement('div');
-//   box.className = className;
-//   return box;
-// }
+// import { useState } from './node_modules/react';
+import {
+  CreateMatriz,
+  CreateClass,
+  addEvent,
+  selectColor,
+  changeColor,
+  clearBoard,
+} from './utilities';
 
-// function CreateMatriz(elementDiv, divLine, divColumn) {
-//   const getElementDiv = document.getElementById(`${elementDiv}`);
-//   for (let i = 0; i < divLine; i += 1) {
-//     const line = createBox('tr');
-//     getElementDiv.appendChild(line);
-//     for (let j = 0; j < divColumn; j += 1) {
-//       const column = createBox('td');
-//       line.appendChild(column);
-//     }
-//   }
-// }
-
-function createClass(elementId, elementsClass, className) {
-  const getClassName = document.getElementById(`${elementId}`).getElementsByClassName(`${elementsClass}`);
-  for (let index = 0; index < getClassName.length; index += 1) {
-    getClassName[index].className += ` ${className}`;
-  }
-}
+// const [numberOfColors, setNumberOfColors] = useState(10);
+const numberOfColors = 10;
 
 const submitBtn = document.querySelector('#send');
-let numberOfColors = 10;
 
 submitBtn.addEventListener('click', () => {
   const colorQuantity = document.querySelector('#color-quantity');
-  const numberOfColors = parseInt(colorQuantity.value);
-  console.log(numberOfColors);
-  // createMatriz('color-palette', 1, numberOfColors);
-
-  location.reload();
+  // eslint-disable-next-line radix
+  // setNumberOfColors(parseInt(colorQuantity.value));
+  console.log(colorQuantity);
 });
 
 CreateMatriz('color-palette', 1, numberOfColors);
-createClass('color-palette', 'td', 'color');
+CreateClass('color-palette', 'td', 'color');
 const boxColor = document.getElementsByClassName('color');
 boxColor[0].style.backgroundColor = 'white';
 boxColor[0].className += ' selected';
@@ -51,44 +35,8 @@ for (let index = 1; index < boxColor.length; index += 1) {
 }
 
 CreateMatriz('pixel-board', 24, 24);
-createClass('pixel-board', 'td', 'pixel');
+CreateClass('pixel-board', 'td', 'pixel');
 
-function addEvent(className, eventName, functionSelected) {
-  const classList = document.getElementsByClassName(`${className}`);
-  for (let index = 0; index < classList.length; index += 1) {
-    classList[index].addEventListener(`${eventName}`, functionSelected);
-  }
-}
-
-function verificaSelected(nameClass) {
-  const listColor = document.getElementsByClassName(`${nameClass}`);
-  for (let index = 0; index < listColor.length; index += 1) {
-    const positionClass = listColor[index].classList;
-    if (positionClass[2] === 'selected') {
-      listColor[index].className = `${positionClass[0]} ${positionClass[1]}`;
-    }
-  }
-}
-
-function selectColor(event) {
-  verificaSelected('color');
-  const divTarget = event.target;
-  divTarget.className += ' selected';
-  console.log(divTarget);
-}
-
-function changeColor(event) {
-  const divTarget = event.target;
-  const color = document.getElementsByClassName('selected')[0].style.backgroundColor;
-  divTarget.style.backgroundColor = `${color}`;
-}
-
-function clearBoard() {
-  const listPixel = document.getElementsByClassName('pixel');
-  for (let index = 0; index < listPixel.length; index += 1) {
-    listPixel[index].style.backgroundColor = 'white';
-  }
-}
 addEvent('pixel', 'click', changeColor);
 addEvent('color', 'click', selectColor);
 document.getElementById('clear-board').addEventListener('click', clearBoard);
--- utilities.js
+++ utilities.js
@@ -15,5 +15,56 @@ function CreateMatriz(elementDiv, divLine, divColumn) {
     }
   }
 }
+function CreateClass(elementId, elementsClass, className) {
+  const getClassName = document.getElementById(`${elementId}`)
+    .getElementsByClassName(`${elementsClass}`);
+  for (let index = 0; index < getClassName.length; index += 1) {
+    getClassName[index].className += ` ${className}`;
+  }
+}
 
-export default CreateMatriz;
+function addEvent(className, eventName, functionSelected) {
+  const classList = document.getElementsByClassName(`${className}`);
+  for (let index = 0; index < classList.length; index += 1) {
+    classList[index].addEventListener(`${eventName}`, functionSelected);
+  }
+}
+
+function verificaSelected(nameClass) {
+  const listColor = document.getElementsByClassName(`${nameClass}`);
+  for (let index = 0; index < listColor.length; index += 1) {
+    const positionClass = listColor[index].classList;
+    if (positionClass[2] === 'selected') {
+      listColor[index].className = `${positionClass[0]} ${positionClass[1]}`;
+    }
+  }
+}
+
+function selectColor(event) {
+  verificaSelected('color');
+  const divTarget = event.target;
+  divTarget.className += ' selected';
+  console.log(divTarget);
+}
+
+function changeColor(event) {
+  const divTarget = event.target;
+  const color = document.getElementsByClassName('selected')[0].style.backgroundColor;
+  divTarget.style.backgroundColor = `${color}`;
+}
+
+function clearBoard() {
+  const listPixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < listPixel.length; index += 1) {
+    listPixel[index].style.backgroundColor = 'white';
+  }
+}
+
+export {
+  CreateMatriz,
+  CreateClass,
+  addEvent,
+  selectColor,
+  changeColor,
+  clearBoard,
+};
